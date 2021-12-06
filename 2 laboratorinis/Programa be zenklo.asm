7c9c0ea8cc2f01e7ba4cc2e6c54dcda08544e999
Martynas Kemežys <60730890+Martynuxas@users.noreply.github.com>
2021-12-06 17:32:23 +0200

    Add files via upload

---
+++ 2 laboratorinis/Programa be zenklo.asm
@@ -0,0 +1,201 @@
+;
+; formule: 4, duomenys: 9
+; suskaiciuoti     /   |x|+2a, 	kai x+c<0
+;              y = |   4b-c^2, 	kai x+c=0
+;                  \   ]c+b/x+c[, kai x+c>0
+; skaiciai be zenklu
+; Duomenys a - w, b - w, c - b, x - w, y - b  
+
+stekas  SEGMENT STACK
+	DB 256 DUP(0)
+stekas  ENDS
+
+duom    SEGMENT 
+a	DW  10  ;   a suteike skaitine reikšmę 2
+b	DW 10	; b suteikia bit tipo skaitine reikšmę -2
+c	DB 8	; c suteikia 2
+x	DW 1,2,1,4,1,9,4,6 ;suteikia daug reikšmių
+kiek	= ($-x)/2	; parodo kiek yra x elementų, $ - galutinis atminties adresas
+y	DB kiek dup(0AAh)	; parodo nuo kur pradeti darba    
+isvb	DB 'x=',6 dup (?), ' y=',6 dup (?), 0Dh, 0Ah, '$'	; išvedimo funkcija
+perp	DB 'Perpildymas', 0Dh, 0Ah, '$'
+daln	DB 'Dalyba is nulio', 0Dh, 0Ah, '$'
+netb	DB 'Netelpa i baita', 0Dh, 0Ah, '$'
+spausk  DB 'Skaiciavimas baigtas, spausk bet kuri klavisa,', 0Dh, 0Ah, '$' 
+duom    ENDS
+
+prog    SEGMENT
+	assume ss:stekas, ds:duom, cs:prog	; specializuoti registrai
+pr:	MOV ax, duom
+	MOV ds, ax
+	XOR si, si      ; (suma mod 2) si = 0
+	XOR di, di      ; di = 0
+c_pr:   MOV cx, kiek
+        JCXZ pab ; jei CX 0, šoka į kažkuria vieta
+cikl:
+	MOV al, b ; į al registra įrašo baita
+	CBW   ; baita praplėčia/apjunge į žodį(W)
+	CMP x[si], ax	; palygina abudu šiu atveju x[si] > ax, -1 > -2
+	JE f2	; jump equal
+	JL f3	; jump in lowest
+f1:	MOV ax, c ; c į ax
+	IMUL c	; dx:ax=c^2, be ženklo MUL, su ženklu IMUL
+	JO kl1  ; sandauga netilpo i ax, persipildo
+	XCHG ax, dx	; sukeičia vietom į dx c^2 į ax 0
+	MOV al, b	; b įkelia į al
+	CBW    ;
+	ADD dx, ax	; c^2+b  sudėtis ir įsirašo į dx
+	JO kl1		; žiūri ar tilpo
+	MOV bx, x[si]	; į bx registra įrašo x[0]
+	SUB bx, ax	; x-b minusuoja, rezultatas į bx
+	JO kl1		; tikrina ar neperpildo
+	CMP bx, 0	; lygina ar bx nėra lygus 0, ar nebus dalybos iš 0
+	JE kl2	; jei dalyba is 0   
+	MOV ax, dx	; dx į ax
+	CWD 		; konvertuoja žodį(W) į double-, tam kad dalybai reik daugiau
+	IDIV bx	; ax=rez
+	JMP re  	
+f2:	MOV ax, 7
+	IMUL a
+	JO kl1  ; sandauga netilpo i ax 
+	SUB ax, x[si] ;7a-x
+	JO kl1
+	JMP re    
+f3:	MOV ax, 2
+	IMUL a
+	JO kl1  ; sandauga netilpo i ax 
+	MOV bx, c
+	CMP bx, 0
+	JG mod       ; jei c < 0 keicia zenkla
+	NEG bx
+mod:	ADD ax, bx ;2a+|c|
+	JO kl1
+re:	
+	CMP al, 0
+	JGE teigr 
+	CMP ah, 0FFh  ; jei neig. rezultatas
+	JE  ger
+	JMP kl3
+teigr:  CMP ah, 0     ;jei teig. rezultatas
+        JE ger	
+	JMP kl3
+ger:	MOV y[di], al
+	INC si
+	INC si
+	INC di
+	LOOP cikl
+pab:	    
+;rezultatu isvedimas i ekrana	            
+;============================
+	XOR si, si
+	XOR di, di 
+        MOV cx, kiek
+        JCXZ is_pab
+is_cikl:
+	MOV ax, x[si]  ; isvedamas skaicius x yra ax reg. 
+	PUSH ax
+	MOV bx, offset isvb+2  
+	PUSH bx
+	CALL binasc
+	MOV al, y[di]
+	CBW		; isvedamas skaicius y yra ax reg. 
+	PUSH ax
+	MOV bx, offset isvb+11 
+	PUSH bx
+	CALL binasc
+	  
+	MOV dx, offset isvb
+	MOV ah, 9h
+	INT 21h 
+;============================
+	INC si     
+	INC si
+	INC di
+	LOOP is_cikl
+is_pab:	
+;===== PAUZE ===================  
+;===== paspausti bet kuri klavisa ===
+	LEA dx, spausk
+	MOV ah, 9
+	INT 21h
+	MOV ah, 0 
+        INT 16h 
+;============================        
+        MOV ah, 4Ch   ; programos pabaiga, grizti i OS
+	INT 21h
+;============================	
+	
+kl1:	LEA dx, perp
+	MOV ah, 9
+	INT 21h
+	XOR al, al
+	JMP ger
+kl2:	LEA dx, daln
+	MOV ah, 9
+	INT 21h
+	XOR al, al
+	JMP ger
+kl3:	LEA dx, netb
+	MOV ah, 9
+	INT 21h
+	XOR al, al
+	JMP ger
+
+; skaiciu vercia i desimtaine sist. ir issaugo
+; ASCII kode. Parametrai perduodami per steka
+; Pirmasis parametras ([bp+6])- verciamas skaicius
+; Antrasis parametras ([bp+4])- vieta rezultatui
+
+binasc	PROC NEAR   
+	PUSH bp
+	MOV bp, sp
+; naudojamu registru issaugojimas
+	PUSHA  
+; rezultato eilute uzpildome tarpais
+	MOV cx, 6   
+	MOV bx, [bp+4]
+tarp:	MOV byte ptr[bx], ' '
+	INC bx
+	LOOP tarp
+; skaicius paruosiamas dalybai is 10   
+	MOV ax, [bp+6]
+	MOV si, 10
+	CMP ax, 0
+	JGE val
+; verciamas skaicius yra neigiamas
+	NEG ax
+val:	XOR dx, dx
+	DIV si
+;  gauta liekana verciame i ASCII koda
+	ADD dx, '0'   ; galima--> ADD dx, 30h
+;  irasome skaitmeni i eilutes pabaiga
+	DEC bx
+	MOV [bx], dl
+; skaiciuojame pervestu simboliu kieki
+	INC cx
+; ar dar reikia kartoti dalyba?
+	CMP ax, 0
+	JNZ val
+; gautas rezultatas. Uzrasome zenkla
+;	pop ax  
+	MOV ax, [bp+6]
+	CMP ax,0
+	JNS teig
+; buvo neigiamas skaicius, uzrasome -
+	DEC bx
+	MOV byte ptr[bx], '-'
+	INC cx
+	JMP vepab
+; buvo teigiamas skaicius, uzrasau +
+teig:	DEC bx
+	MOV byte ptr[bx], '+'
+	INC cx
+vepab: 	
+	POPA  
+	POP bp
+	RET
+binasc	ENDP    
+prog    ENDS 
+        END pr
+
+
